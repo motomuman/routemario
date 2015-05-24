04a50a83ab9c67674c402269caa54bc2b239074d
Motomu Utsumi <motomu@hongo.wide.ad.jp>
2015-05-24 08:17:23 -0400

    ttl

--- main.c
+++ main.c
@@ -247,7 +247,7 @@ static void packet_handle_external(struct rte_mbuf *m, unsigned portid){
           printf("TTL 0 TIME EXCEEDED\n");
           struct rte_mbuf *pkt;
           pkt = rte_pktmbuf_alloc(l2fwd_pktmbuf_pool[rte_lcore_id()]);
-          make_ttl_expkt(m, pkt, ip_hdr->src_addr);
+          make_ttl_expkt(m, pkt, port_to_ip[portid]);
 
 
           TX_enqueue(pkt, (uint8_t) portid);
