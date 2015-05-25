7da7f393278953d881c37d9230122e726ec946c1
Motomu Utsumi <motomu@hongo.wide.ad.jp>
2015-05-25 04:51:24 -0400

    next_set_hash

--- main.c
+++ main.c
@@ -242,10 +242,9 @@ static void packet_handle_external(struct rte_mbuf *m, unsigned portid){
           if(ret >= 0){
             next_set = nextset_table[ret]; 
             printf("out from casche\n");
-
           }else{
             next_set = lookup(rte_bswap32(ip_hdr->dst_addr));
-            ret = rte_hash_add_key(mac_table_hash[portid],(void *) &next_set);
+            ret = rte_hash_add_key(nextset_hash,(void *) &next_set);
             nextset_table[ret] = next_set;
           }
 
